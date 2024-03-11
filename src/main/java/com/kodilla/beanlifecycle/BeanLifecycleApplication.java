d463adc28783560c42b652fbd94048b841119c22
jwypysze <joanna.wypyszewska@gmail.com>
2024-03-11 21:28:39 +0100

    BeanLifecycleApplication class changed

--- src/main/java/com/kodilla/beanlifecycle/BeanLifecycleApplication.java
+++ src/main/java/com/kodilla/beanlifecycle/BeanLifecycleApplication.java
@@ -1,19 +1,23 @@
 package com.kodilla.beanlifecycle;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
+@SpringBootApplication
 public class BeanLifecycleApplication {
 
-	private static TaskPostProcessorOfBeansController t = new TaskPostProcessorOfBeansController();
 
 	public static void main(String[] args) {
+
+		SpringApplication.run(BeanLifecycleApplication.class, args);
 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 		context.register(AppConfiguration.class);
 		context.refresh();
 
 		LibraryManager manager = context.getBean(LibraryManager.class);
+		LibraryManager manager2 = context.getBean(LibraryManager.class);
 		System.out.println("Context and beans are set up and ready to work");
 
 		context.close();
--- src/main/java/com/kodilla/beanlifecycle/TaskPostProcessorOfBeansController.java
+++ src/main/java/com/kodilla/beanlifecycle/TaskPostProcessorOfBeansController.java
@@ -1,12 +1,19 @@
 package com.kodilla.beanlifecycle;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TaskPostProcessorOfBeansController {
+
+    private final LibraryManager libraryManager;
+
+    public TaskPostProcessorOfBeansController(LibraryManager libraryManager) {
+        this.libraryManager = libraryManager;
+    }
+
 
     @ResponseBody
     @RequestMapping(path = "/create", method = RequestMethod.GET)
